c972116623a1b8d91d4f6916b0077e383fa432b6
Dmitry Kobets <cusl2@yahoo.com>
2021-03-09 22:06:45 +0200

    log in unit test

--- src/test/java/lunarfactories/morse/service/tests/unit/MorseServiceUnitTests.java
+++ src/test/java/lunarfactories/morse/service/tests/unit/MorseServiceUnitTests.java
@@ -1,12 +1,14 @@
 package lunarfactories.morse.service.tests.unit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 public class MorseServiceUnitTests {
     @Test
     void translateIp() {
-
+        log.info("I believe that unit tests are passed :)");
     }
 }
